a71624537e44fad6255034528a9d067bd3afc5b5
Petru Mot <petru.mot@it-teams.com>
2020-01-13 01:00:26 +0200

    suites

--- src/test/java/com/ase/unittesting/suites/AllFeatures.java
+++ src/test/java/com/ase/unittesting/suites/AllFeatures.java
@@ -1,8 +1,12 @@
-package com.ase.unittesting.mainfeatures;
+package com.ase.unittesting.suites;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import com.ase.unittesting.mainfeatures.FeatureOneTest;
+import com.ase.unittesting.mainfeatures.FeatureThreeTest;
+import com.ase.unittesting.mainfeatures.FeatureTwoTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ FeatureOneTest.class, FeatureThreeTest.class, FeatureTwoTest.class })
